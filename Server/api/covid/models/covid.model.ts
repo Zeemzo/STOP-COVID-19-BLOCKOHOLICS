c881cf3e83ebfafe3a32bcbabae27958b3c40042
Zeemzo <azeemashraf@outlook.com>
2020-05-04 06:45:13 +0530

    models added to initiatives

--- Server/api/covid/models/covid.model.ts
+++ Server/api/covid/models/covid.model.ts
@@ -1,24 +1,26 @@
 import * as Joi from "@hapi/joi";
 
 export const registerSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  dob: Joi.string().optional(),
+  alias: Joi.string().required(),
+  email: Joi.string().optional().allow(""),
   publicKey: Joi.string().required(),
-  encryptedSecret: Joi.string().required(),
-  recoveryQuestion: Joi.string().required(),
-  encryptedRecoverySecret: Joi.string().required(),
-  pash: Joi.string().required(),
+  encryptedSecret: Joi.string().optional().allow(""),
+  recoveryQuestion: Joi.string().optional().allow(""),
+  encryptedRecoverySecret: Joi.string().optional().allow(""),
+  authType: Joi.number().valid(0,1,2).optional(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().required(),
+  key: Joi.string().required(),
   password: Joi.string().optional(),
 });
 
+export const loginWithSecretSchema = Joi.object({
+  xdr: Joi.string().required(),
+});
 
 export const editUserSchema = Joi.object({
-  name: Joi.string().optional(),
+  alias: Joi.string().optional(),
   email: Joi.string().optional(),
   dob: Joi.string().optional(),
   publicKey: Joi.string().optional(),
@@ -28,6 +30,49 @@ export const editUserSchema = Joi.object({
   pash: Joi.string().optional(),
 });
 
+export const addInitiativeSchema = Joi.object({
+  name: Joi.string().optional(),
+  txnhash: Joi.string().optional(),
+  xdr: Joi.string().optional(),
+  goal: Joi.number().optional(),
+  creator: Joi.string().optional(),
+  recipient: Joi.string().optional(),
+  recipientType: Joi.string().optional(),
+  endDate: Joi.string().optional(),
+  cover: Joi.string().optional(),
+  description: Joi.string().optional(),
+  paymentDetails: Joi.object().optional(),
+});
+
+export const editInitiativeSchema = Joi.object({
+  name: Joi.string().optional(),
+  txnhash: Joi.string().optional(),
+  xdr: Joi.string().optional(),
+  goal: Joi.number().optional(),
+  creator: Joi.string().optional(),
+  recipient: Joi.string().optional(),
+  recipientType: Joi.string().optional(),
+  endDate: Joi.string().optional(),
+  cover: Joi.string().optional(),
+  description: Joi.string().optional(),
+  paymentDetails: Joi.object().optional(),
+});
+
+export class Initiative {
+  _id: String;
+  name: String;
+  txnhash: String;
+  goal: String;
+  creator: String;
+  recipient: String;
+  recipientType: String;
+  endDate: String;
+  cover: String;
+  description: String;
+  paymentDetails: Object;
+  createdAt: String;
+  updatedAt: String;
+}
 
 export class User {
   _id: String;
